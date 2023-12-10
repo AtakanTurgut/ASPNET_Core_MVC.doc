2b8aa0f972e3d14614e9908247f338811ed759ab
AtakanTurgut <77547962+AtakanTurgut@users.noreply.github.com>
2023-12-10 12:31:13 +0300

    ServiceExtension has been completed.

---
+++ Store/StoreApp/Infrastructure/Extensions/ServiceExtension.cs
@@ -0,0 +1,52 @@
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Repositories;
+using Repositories.Contracts;
+using Services;
+using Services.Contracts;
+using StoreApp.Models;
+
+namespace StoreApp.Infrastructure.Extensions
+{
+    public static class ServiceExtension
+    {
+        public static void ConfigureDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<RepositoryContext>(options =>
+            {
+                options.UseSqlite(configuration.GetConnectionString("sqlConnection"),
+                b => b.MigrationsAssembly("StoreApp"));
+            });
+        }
+
+        public static void ConfigureSession(this IServiceCollection services)
+        {
+            services.AddDistributedMemoryCache();
+            services.AddSession(options =>
+            {
+                options.Cookie.Name = "StoreApp.Session";
+                options.IdleTimeout = TimeSpan.FromMinutes(10);  // 10min
+            });
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<Cart>(c => SessionCart.GetCart(c));
+        }
+
+        public static void ConfigureRepositoryRegistration(this IServiceCollection services)
+        {
+            services.AddScoped<IRepositoryManager, RepositoryManage>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+        }
+
+        public static void ConfigureServiceRegistration(this IServiceCollection services)
+        {
+            services.AddScoped<IServiceManager, ServiceManager>();
+            services.AddScoped<IProductService, ProductManager>();
+            services.AddScoped<ICategoryService, CategoryManager>();
+            services.AddScoped<IOrderService, OrderManager>();
+        }
+
+    }
+}
--- Store/StoreApp/Program.cs
+++ Store/StoreApp/Program.cs
@@ -5,6 +5,7 @@ using Repositories;
 using Repositories.Contracts;
 using Services;
 using Services.Contracts;
+using StoreApp.Infrastructure.Extensions;
 using StoreApp.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,37 +17,50 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
 // Database Connection
+/*
 builder.Services.AddDbContext<RepositoryContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("sqlConnection"), b => b.MigrationsAssembly("StoreApp"));
 });
+Extensions */
+builder.Services.ConfigureDbContext(builder.Configuration);
 
 // Session Management
+/*
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession(options => 
 {
     options.Cookie.Name = "StoreApp.Session";
     options.IdleTimeout = TimeSpan.FromMinutes(10);  // 10min
 });
-builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+Extensions */
+builder.Services.ConfigureSession();
+
+//builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
 // Repositories
 // IoC -> Register - Resolve - Dispose
+/*
 builder.Services.AddScoped<IRepositoryManager, RepositoryManage>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+Extensions */
+builder.Services.ConfigureRepositoryRegistration();
 // Session Management
 
 // Services
+/*
 builder.Services.AddScoped<IServiceManager, ServiceManager>();
 builder.Services.AddScoped<IProductService, ProductManager>();
 builder.Services.AddScoped<ICategoryService, CategoryManager>();
 builder.Services.AddScoped<IOrderService, OrderManager>();
+Extensions */
+builder.Services.ConfigureServiceRegistration();
 
 //Razor Pages
 //builder.Services.AddSingleton<Cart>();  // All users have 1 cart.
-builder.Services.AddScoped<Cart>(c => SessionCart.GetCart(c));
+//builder.Services.AddScoped<Cart>(c => SessionCart.GetCart(c));
 
 // AutoMapper
 builder.Services.AddAutoMapper(typeof(Program));
